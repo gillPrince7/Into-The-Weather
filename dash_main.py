4f893f23f845f3566c456504ce56cc2b6d06ddde
Parminder Singh Gill <133721736+gillPrince7@users.noreply.github.com>
2023-11-25 17:27:33 -0500

    Update dash_main.py

--- dash_main.py
+++ dash_main.py
@@ -330,7 +330,7 @@ elif selected_page == "About":
     st.write(" ")
 
     team_members = [
-        {"name": "Marco", "role": "Team Lead", "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.","photo_url": "https://github.com/gillPrince7/Into-The-Weather/blob/main/photos/marco.jpeg",},
+        {"name": "Marco", "role": "Team Lead", "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.","photo_url": "https://github.com/gillPrince7/Into-The-Weather/blob/main/photos/marco.JPEG",},
         {"name": "Surya", "role": "Lead Developer", "bio": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.","photo_url": "https://github.com/gillPrince7/Into-The-Weather/blob/main/photos/surya.jpeg",},
         {"name": "Parminder", "role": "Designer", "bio": "Nulla facilisi. Vivamus euismod, odio nec bibendum ullamcorper, ligula dui scelerisque neque.","photo_url": "https://github.com/gillPrince7/Into-The-Weather/blob/main/photos/gill.jpeg",},
         {"name": "Barinder", "role": "Car Specialist", "bio": "Nulla facilisi. Vivamus euismod, odio nec bibendum ullamcorper, ligula dui scelerisque neque.","photo_url": "https://github.com/gillPrince7/Into-The-Weather/blob/main/photos/bari.jpeg",},
